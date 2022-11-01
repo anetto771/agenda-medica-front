a324b16cf85401c82e312c601c06cc17695dbd12
Netto <anetto771@gmail.com>
2022-11-01 18:43:40 -0300

    criacao de componentes

---
+++ agenda-medica-front/src/app/components/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [
+    HomeComponent
+  ]
+})
+export class HomeModule { }
